981f76a54ede18266be622c5a25f9cead46483fd
simi.joseph7@hotmail.com <simi.joseph7@hotmail.com>
2022-11-01 01:29:51 +1300

    End of Section 2 - API

--- StudentAdminPortal.API/StudentAdminPortal.API/Program.cs
+++ StudentAdminPortal.API/StudentAdminPortal.API/Program.cs
@@ -1,11 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using StudentAdminPortal.API.DataModels;
+using StudentAdminPortal.API.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+//Add DbContext
+builder.Services.AddDbContext<StudentAdminContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("StudentAdminPortalDb")));
+//Add Repository
+builder.Services.AddScoped<IStudentRepository, SqlStudentRepository>();
+
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Injecting Automapper inside application
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 var app = builder.Build();
 
